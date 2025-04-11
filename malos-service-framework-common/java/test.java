fd70479419ba108fb55459b8548cb0c30635c6bb
YANGYUNAN <YANGYUNAN@jp.cn>
2025-04-11 12:10:36 +0900

    1

--- malos-service-framework-common/java/test.java
+++ malos-service-framework-common/java/test.java
@@ -1,5 +1,5 @@
 public class test {
-    private String abcc-main ;
+    private String yangyunan-main ;
     private String abcd-main ;
     private String abcc-2025 ;
     private String abcd-2025 ;
